70ed78fdb73f793249a8d3b443e341992acba3e4
Prachit Amin <prachitamin12@gmail.com>
2025-09-29 23:50:03 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+
+// TODO: 3 functions for the game of life:
+// sequential, threaded, OpenMP
+// RNG for setting grid element to alive or dead -> enum for each grid value
+// continuous generation up until ESC key or window close
+// draw graphics and output processing time
+
+
+
+// accomodate CLA
+// note the cases for threading selection
+int main () {
+    
+    return 0;
+}
