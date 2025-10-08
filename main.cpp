1cf1da0d755451be303b724537ead499086405e8
Prachit Amin <prachitamin12@gmail.com>
2025-10-08 09:22:24 -0400

    Working sequential game of life implementation

--- main.cpp
+++ main.cpp
@@ -11,157 +11,191 @@
 
 std::vector<std::vector<int>> lifeSEQ(std::vector<std::vector<int>> board, int x, int y)
 {
-    // check how the cell SHOULD change, update afterwards to maintain "simultaneous" behavior
-    // temporary board
-    std::vector<std::vector<int>> tBoard(x, std::vector<int>(y));
-    for (int i = 0; i < board.size(); i++)
+  // check how the cell SHOULD change, update afterwards to maintain "simultaneous" behavior
+  // temporary board
+  std::vector<std::vector<int>> tBoard(x, std::vector<int>(y));
+  for (int i = 0; i < board.size(); i++)
+  {
+    for (int j = 0; j < board[i].size(); j++)
     {
-        for (int j = 0; j < board[i].size(); j++)
-        {
-            int countAlive = 0;
-            if (i > 0 && j > 0 && board[i - 1][j - 1] == 1)
-            {
-                countAlive++; // top left
-            }
-            if (i > 0 && board[i - 1][j] == 1)
-            {
-                countAlive++; // top
-            }
-            if (i > 0 && j < board[i].size() - 1 && board[i - 1][j + 1] == 1)
-            {
-                countAlive++; // top right
-            }
-            if (j > 0 && board[i][j - 1] == 1)
-            {
-                countAlive++; // left
-            }
-            if (j < board[i].size() - 1 && board[i][j + 1] == 1)
-            {
-                countAlive++; // right
-            }
-            if (i < board.size() - 1 && j > 0 && board[i + 1][j - 1] == 1)
-            {
-                countAlive++; // bottom left
-            }
-            if (i < board.size() - 1 && board[i + 1][j] == 1)
-            {
-                countAlive++; // bottom
-            }
-            if (i < board.size() - 1 && j < board[i].size() - 1 && board[i + 1][j + 1] == 1)
-            {
-                countAlive++; // bottom right
-            }
+      int countAlive = 0;
+      if (i > 0 && j > 0 && board[i - 1][j - 1] == 1)
+      {
+        countAlive++; // top left
+      }
+      if (i > 0 && board[i - 1][j] == 1)
+      {
+        countAlive++; // top
+      }
+      if (i > 0 && j < board[i].size() - 1 && board[i - 1][j + 1] == 1)
+      {
+        countAlive++; // top right
+      }
+      if (j > 0 && board[i][j - 1] == 1)
+      {
+        countAlive++; // left
+      }
+      if (j < board[i].size() - 1 && board[i][j + 1] == 1)
+      {
+        countAlive++; // right
+      }
+      if (i < board.size() - 1 && j > 0 && board[i + 1][j - 1] == 1)
+      {
+        countAlive++; // bottom left
+      }
+      if (i < board.size() - 1 && board[i + 1][j] == 1)
+      {
+        countAlive++; // bottom
+      }
+      if (i < board.size() - 1 && j < board[i].size() - 1 && board[i + 1][j + 1] == 1)
+      {
+        countAlive++; // bottom right
+      }
 
-            // adjust the temp board according to the rules
-            if (board[i][j] == 1 && (countAlive == 2 || countAlive == 3))
-            { // if the cell is alive and has 2/3 neighbors alive
-                tBoard[i][j] = 1;
-            }
-            else if (board[i][j] == 0 && countAlive == 3)
-            {
-                tBoard[i][j] = 1;
-            }
-            else
-            {
-                tBoard[i][j] = 0;
-            }
-        }
+      // adjust the temp board according to the rules
+      if (board[i][j] == 1 && (countAlive == 2 || countAlive == 3))
+      { // if the cell is alive and has 2/3 neighbors alive
+        tBoard[i][j] = 1;
+      }
+      else if (board[i][j] == 0 && countAlive == 3)
+      {
+        tBoard[i][j] = 1;
+      }
+      else
+      {
+        tBoard[i][j] = 0;
+      }
     }
-    return tBoard;
+  }
+  return tBoard;
 }
 
 // accomodate CLA
 // note the cases for threading selection
 int main(int argc, char *argv[])
 {
-    int threads = 2;
-    int cellSize = 5, width = 800, height = 600;
-    int t = 1; // 0 -> SEQ, 1 -> THRD, 2 -> OMP
+  int threads = 2;
+  int cellSize = 5, width = 800, height = 600;
+  int t = 1; // 0 -> SEQ, 1 -> THRD, 2 -> OMP
 
-    // TODO: Revise the flag parsing? use a library?
-    // TODO: Add error checking
-    for (int i = 0; i < argc; i++)
+  // TODO: Revise the flag parsing? use a library?
+  // TODO: Add error checking
+  for (int i = 0; i < argc; i++)
+  {
+    std::string arg = argv[i];
+    // auto nextArg = (i + 1 != argc) ? argv[i + 1] : "";
+    auto nextArg = argv[i + 1];
+    if (arg == "-n")
     {
-        std::string arg = argv[i];
-        // auto nextArg = (i + 1 != argc) ? argv[i + 1] : "";
-        auto nextArg = argv[i + 1];
-        if (arg == "-n")
-        {
-            threads = atoi(nextArg);
-        }
-        else if (arg == "-c")
-        {
-            cellSize = atoi(nextArg);
-        }
-        else if (arg == "-x")
-        {
-            width = atoi(nextArg);
-        }
-        else if (arg == "-y")
-        {
-            height = atoi(nextArg);
-        }
-        else if (arg == "-t")
-        {
-            std::cout << nextArg << "\n";
-            if ((std::string)(nextArg) == "SEQ")
-            {
-                t = 0;
-            }
-            else if ((std::string)nextArg == "THRD")
-            {
-                t = 1;
-            }
-            else if ((std::string)nextArg == "OMP")
-            {
-                t = 2;
-            }
-        }
+      threads = atoi(nextArg);
     }
-    std::cout << threads << " " << cellSize << " "
-              << width << " "
-              << height << " "
-              << t << " "
-              << "\n";
-
-    // int xCells = width / cellSize;
-    // int yCells = height / cellSize;
-
-    int xCells = 5;
-    int yCells = 5;
-    // std::vector<std::vector<int>> board; // initialize board only to the size required
-    std::vector<std::vector<int>> board(xCells, std::vector<int>(yCells));
-    std::vector<std::vector<int>> tBoard(xCells, std::vector<int>(yCells));
-
-    // initialize randomness of the board
-    std::mt19937 rng{std::random_device{}()}; // ~true randomness
-    for (int i = 0; i < xCells; i++)
+    else if (arg == "-c")
     {
-        for (int j = 0; j < yCells; j++)
-        {
-            board[i][j] = rng() % 2; // mod 2 always [0,1], 0 = dead, 1 = alive
-            std::cout << board[i][j] << " ";
-        }
-        std::cout << "\n";
+      cellSize = atoi(nextArg);
     }
-
-    // TODO: add SFML context for the game play
-    // should have set clock timing and esc key condition
-    switch (t)
+    else if (arg == "-x")
     {
-    case 0:
-        board = lifeSEQ(board, xCells, yCells);
+      width = atoi(nextArg);
+    }
+    else if (arg == "-y")
+    {
+      height = atoi(nextArg);
+    }
+    else if (arg == "-t")
+    {
+      std::cout << nextArg << "\n";
+      if ((std::string)(nextArg) == "SEQ")
+      {
+        t = 0;
+      }
+      else if ((std::string)nextArg == "THRD")
+      {
+        t = 1;
+      }
+      else if ((std::string)nextArg == "OMP")
+      {
+        t = 2;
+      }
+    }
+  }
+  std::cout << threads << " " << cellSize << " "
+            << width << " "
+            << height << " "
+            << t << " "
+            << "\n";
+
+  int xCells = width / cellSize;
+  int yCells = height / cellSize;
+
+  //   int xCells = 5;
+  //   int yCells = 5;
+  // std::vector<std::vector<int>> board; // initialize board only to the size required
+  std::vector<std::vector<int>> board(xCells, std::vector<int>(yCells));
+  std::vector<std::vector<int>> tBoard(xCells, std::vector<int>(yCells));
+
+  // initialize randomness of the board
+  std::mt19937 rng{std::random_device{}()}; // ~true randomness
+  for (int i = 0; i < xCells; i++)
+  {
+    for (int j = 0; j < yCells; j++)
+    {
+      board[i][j] = rng() % 2; // mod 2 always [0,1], 0 = dead, 1 = alive
+      std::cout << board[i][j];
     }
     std::cout << "\n";
+  }
 
-    for (int i = 0; i < xCells; i++)
+  // create variable sized window for the game
+  sf::RenderWindow window(sf::VideoMode(width, height), "Game of Life");
+  sf::RectangleShape rect(sf::Vector2f(width, height));
+  sf::Clock clk;
+  clk.restart();
+  rect.setFillColor(sf::Color::Black);
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        for (int j = 0; j < yCells; j++)
-        {
-            std::cout << board[i][j] << " ";
-        }
-        std::cout << "\n";
+      switch (event.type)
+      {
+        case sf::Event::Closed:
+          window.close();
+          break;
+        case sf::Event::KeyPressed:
+          if (event.key.code == sf::Keyboard::Escape)
+          {
+            window.close();
+            break;
+          }
+      }
     }
 
-    return 0;
+    if (clk.getElapsedTime().asSeconds() >= 1.0)
+    {
+      // switch case to determine which game type to run
+      switch (t)
+      {
+        case 0:
+          board = lifeSEQ(board, xCells, yCells);
+          // add other cases
+      }
+      clk.restart();
+    }
+
+    // draw the tiles
+    window.clear();
+    window.draw(rect);
+    for (int i = 0; i < xCells; i++)
+    {
+      for (int j = 0; j < yCells; j++)
+      {
+        sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
+        cell.setPosition((cellSize * i), (cellSize * j));
+        cell.setFillColor(board[i][j] == 1 ? sf::Color::White : sf::Color::Black);
+        window.draw(cell);
+      }
+    }
+    window.display();
+  }
+  return 0;
 }
